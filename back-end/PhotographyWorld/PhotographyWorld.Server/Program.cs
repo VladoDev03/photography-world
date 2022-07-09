b1df886ea1c79bfecc2bd6ee2ad9f037dc2e2c53
VladoDev03 <vladsto101@gmail.com>
2022-07-10 02:40:14 +0300

    Changed CORS Policy of the back-end.

--- back-end/PhotographyWorld/PhotographyWorld.Server/Program.cs
+++ back-end/PhotographyWorld/PhotographyWorld.Server/Program.cs
@@ -1,15 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+const string AllowedSpecificOrigins = "AllowedPolicies";
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: AllowedSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:3000")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                      });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -19,6 +28,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(AllowedSpecificOrigins);
 
 app.MapControllers();
 
