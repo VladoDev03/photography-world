e9d3e3f1a051ad02b0f66e74690a2b799ef9f7f9
VladoDev03 <vladsto101@gmail.com>
2022-07-22 00:11:42 +0300

    Removed login.module.css and register.module.css

--- front-end/photography-world/src/components/register/Register.js
+++ front-end/photography-world/src/components/register/Register.js
@@ -1,7 +1,6 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { Link } from 'react-router-dom'
-// import styles from './Register.module.css'
 
 export function Register() {
     const navigate = useNavigate()
