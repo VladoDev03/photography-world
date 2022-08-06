39e51da7b7dcb543664fb60da84af2b98befc4ac
VladoDev03 <vladsto101@gmail.com>
2022-08-07 01:08:41 +0300

    Added order styles.

--- front-end/photography-world/src/components/profile/Profile.js
+++ front-end/photography-world/src/components/profile/Profile.js
@@ -13,7 +13,7 @@ export function Profile() {
     const [userImages, setUserImages] = useState([])
     const { user } = useContext(AuthContext)
     const { id } = useParams()
-    const [order, setOrder] = useLocalStorage('type', { type: 'date' })
+    const [order, setOrder] = useLocalStorage('ordering', { type: 'date' })
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -31,7 +31,7 @@ export function Profile() {
                 setUserImages(pictureOrdering.orderByDescription(data.pictures))
             }
         }).catch(() => navigate("/*"))
-    }, [user, id, navigate])
+    }, [user, id, order, navigate])
 
     const orderByDescriptionHandler = () => {
         const sortedList = pictureOrdering.orderByDescription(userImages)
@@ -49,8 +49,8 @@ export function Profile() {
         <div className={styles['container']}>
             <ul className={styles['criteria-list']}>
                 <li><h3 className={styles['sort-title']}>Order by:</h3></li>
-                <li><Link onClick={orderByDateHandler} className={styles['criteria']} to='?order=date' replace>Date</Link></li>
-                <li><Link onClick={orderByDescriptionHandler} className={styles['criteria']} to='?order=description' replace>Description</Link></li>
+                <li><Link onClick={orderByDateHandler} className={`${styles['criteria']} ${order.type === 'date' ? styles['active-criteria'] : ''}`} to='?order=date' replace>Date</Link></li>
+                <li><Link onClick={orderByDescriptionHandler} className={`${styles['criteria']} ${order.type === 'description' ? styles['active-criteria'] : ''}`} to='?order=description' replace>Description</Link></li>
             </ul>
             <h1 className={styles['profile-title']}>{displayUser.username}</h1>
             <Gallery>{userImages.map(x => <Image key={x.id} id={x.id} src={x.url} content={x.description} />)}</Gallery>
