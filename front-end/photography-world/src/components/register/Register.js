e5c97289a8f1a762d32ef4f78161ffa8f84fa3cb
VladoDev03 <vladsto101@gmail.com>
2022-08-08 01:03:08 +0300

    Added handling errors from the server for register.

--- front-end/photography-world/src/components/register/Register.js
+++ front-end/photography-world/src/components/register/Register.js
@@ -9,6 +9,7 @@ export function Register() {
     const navigate = useNavigate()
     const { userLogin } = useContext(AuthContext)
     const [errors, setErrors] = useState({})
+    const [responseErrors, setResponseErrors] = useState({})
 
     const [values, setValues] = useState({
         username: '',
@@ -61,44 +62,53 @@ export function Register() {
             confirmPassword: values.confirmPassword
         }
 
-        authService.register(userData).then(data => userLogin(data))
-
-        navigate('/login')
+        authService.register(userData)
+            .then(data => {
+                userLogin(data)
+                if (!data.message) {
+                    navigate('/login')
+                }
+                setResponseErrors(oldErrors => ({
+                    ...oldErrors,
+                    ['existingUser']: data.message
+                }))
+            })
     }
 
     return (
         <>
-        {/* {errors.usernameLength ? <span className='error-message'>Username must be at least 3 characters long</span> : ''} */}
-        {/* {errors.passwordLength ? <span className='error-message'>Password must be at least 7 symbols long</span> : ''} */}
-        {/* {errors.passwordUpper ? <span className='error-message'>Password must contain at least one upper case letter</span> : ''} */}
-        {/* {errors.passwordLower ? <span className='error-message'>Password must contain at least one lower case letter</span> : ''} */}
-        {/* {errors.passwordSymbol ? <span className='error-message'>Password must contain at least one symbol</span> : ''} */}
-        {/* {errors.passwordNumber ? <span className='error-message'>Password must contain at least one digit</span> : ''} */}
-        {/* {errors.passwordsMatch ? <span className='error-message'>Passwords must match</span> : ''} */}
-        <div className='center'>
-            <h1>Register</h1>
-            <form method="post" onSubmit={submitHandler}>
-                <div className='text-field'>
-                    <input name="username" type="text" value={values.username} onChange={changeHandler} onBlur={usernameValidate} required />
-                    <label htmlFor="username">Username</label>
-                    <span></span>
-                </div>
-                <div className='text-field'>
-                    <input name="password" type="password" value={values.password} onChange={changeHandler} onBlur={passwordValidate} required />
-                    <label htmlFor="password">Password</label>
-                    <span></span>
-                </div>
-                <div className='text-field'>
-                    <input name="confirmPassword" type="password" value={values.confirmPassword} onChange={changeHandler} onBlur={confirmPasswordValidate} required />
-                    <label htmlFor="confirmPassword">Confirm Password</label>
-                    <span></span>
-                </div>
-                <input type="submit" value="Register" disabled={Object.values(errors).some(x => x)} />
-                <div className='signup-link'>
-                    Already have an account? <Link to="/login">Login</Link>
-                </div>
-            </form>
-        </div>
+            {/* {errors.usernameLength ? <span className='error-message'>Username must be at least 3 characters long</span> : ''} */}
+            {/* {errors.passwordLength ? <span className='error-message'>Password must be at least 7 symbols long</span> : ''} */}
+            {/* {errors.passwordUpper ? <span className='error-message'>Password must contain at least one upper case letter</span> : ''} */}
+            {/* {errors.passwordLower ? <span className='error-message'>Password must contain at least one lower case letter</span> : ''} */}
+            {/* {errors.passwordSymbol ? <span className='error-message'>Password must contain at least one symbol</span> : ''} */}
+            {/* {errors.passwordNumber ? <span className='error-message'>Password must contain at least one digit</span> : ''} */}
+            {/* {errors.passwordsMatch ? <span className='error-message'>Passwords must match</span> : ''} */}
+            {/* {responseErrors.existingUser} */}
+            <div className='center'>
+                <h1>Register</h1>
+                <form method="post" onSubmit={submitHandler}>
+                    <div className='text-field'>
+                        <input name="username" type="text" value={values.username} onChange={changeHandler} onBlur={usernameValidate} required />
+                        <label htmlFor="username">Username</label>
+                        <span className="blue-underline"></span>
+                    </div>
+                    <div className='text-field'>
+                        <input name="password" type="password" value={values.password} onChange={changeHandler} onBlur={passwordValidate} required />
+                        <label htmlFor="password">Password</label>
+                        <span className="blue-underline"></span>
+                    </div>
+                    <div className='text-field'>
+                        <input name="confirmPassword" type="password" value={values.confirmPassword} onChange={changeHandler} onBlur={confirmPasswordValidate} required />
+                        <label htmlFor="confirmPassword">Confirm Password</label>
+                        <span className="blue-underline"></span>
+                    </div>
+                    <input type="submit" value="Register" disabled={Object.values(errors).some(x => x)} />
+                    <div className='signup-link'>
+                        Already have an account? <Link to="/login">Login</Link>
+                    </div>
+                </form>
+            </div>
         </>
     )
 }
