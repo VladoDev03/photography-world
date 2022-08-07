1c3e69f6ca34454e240d9b6ace91218aa1fab785
VladoDev03 <vladsto101@gmail.com>
2022-08-07 04:04:12 +0300

    Added localhost.

--- back-end/PhotographyWorld/PhotographyWorld.Server/Program.cs
+++ back-end/PhotographyWorld/PhotographyWorld.Server/Program.cs
@@ -53,7 +53,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           policy
-                            .WithOrigins("https://photography-world-81449.web.app")
+                            .WithOrigins("https://photography-world-81449.web.app", "http://localhost:3000")
                             .AllowAnyMethod()
                             .AllowAnyHeader();
                       });
