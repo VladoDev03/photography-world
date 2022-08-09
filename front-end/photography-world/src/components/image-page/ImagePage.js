7804305ed23f67ba92704c55ef636e51e349b5e3
VladoDev03 <vladsto101@gmail.com>
2022-08-09 03:37:09 +0300

    Added image modal on image page that opens when image is clicked.

--- front-end/photography-world/src/components/image-page/ImagePage.js
+++ front-end/photography-world/src/components/image-page/ImagePage.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react"
-import { useParams, useNavigate, Link } from 'react-router-dom'
+import { useParams, useNavigate, useSearchParams, Link } from 'react-router-dom'
 import { ConfirmDelete } from '../confirm-delete/ConfirmDelete'
 import { EditImage } from "../edit-image/EditImage"
 import { LoadingSpinner } from "../loading-spinner/LoadingSpinner"
@@ -15,15 +15,25 @@ export function ImagePage() {
     const [isEditing, setIsEditing] = useState(false)
     const [isOwner, setIsOwner] = useState(false)
     const [isLoading, setIsLoading] = useState(false)
+    const [isOverviewOpen, setIsOverviewOpen] = useState(false)
     const { user } = useContext(AuthContext)
     const { id } = useParams()
+    const [overviewParams, setOverviewParams] = useSearchParams()
     const navigate = useNavigate()
 
     useEffect(() => {
+        let isOverview = overviewParams.get('overview') || 'false'
+
+        if (isOverview === 'true') {
+            setIsOverviewOpen(true)
+        }
+
+        setOverviewParams({ overview: isOverview }, { replace: true })
+
         imageServices.getImageById(id).then(data => {
             setImage(data.url)
             setDescription(data.description)
-            setUserDetails({userId: data.user.id, username: data.user.username})
+            setUserDetails({ userId: data.user.id, username: data.user.username })
             setIsOwner(data.user.id === user.user.id)
         })
     }, [])
@@ -36,9 +46,14 @@ export function ImagePage() {
         setIsEditing(true)
     }
 
-    const closeHandler = (e) => {
+    const closeHandler = () => {
         setIsAsked(false)
         setIsEditing(false)
+    }
+
+    const closeOverviewHandler = () => {
+        setIsOverviewOpen(false)
+        setOverviewParams({ overview: false }, { replace: true })
     }
 
     const deleteHandler = () => {
@@ -50,6 +65,11 @@ export function ImagePage() {
         })
     }
 
+    const imgClickHandler = () => {
+        setIsOverviewOpen(true)
+        setOverviewParams({ overview: true }, { replace: true })
+    }
+
     return (
         <div className='container'>
             {isEditing ? <EditImage setIsLoading={setIsLoading} setDescription={setDescription} description={description} imageId={id} closeHandler={closeHandler} /> : ''}
@@ -59,7 +79,7 @@ export function ImagePage() {
                     <LoadingSpinner />
                 </div> : ''}
             <div className={styles['image-data']}>
-                <img className={styles['image']} src={image} />
+                <img className={styles['image']} src={image} onClick={imgClickHandler} />
                 <div>
                     <p className={styles['content']}>
                         <span className={styles['property-name']}>Photographer: </span>
@@ -71,6 +91,10 @@ export function ImagePage() {
                     </p>
                 </div>
             </div>
+            {isOverviewOpen ?
+                <div onClick={closeOverviewHandler} className={styles['overview-container']}>
+                    <img onClick={e => e.stopPropagation()} className={styles['image-overview']} src={image} />
+                </div> : ''}
             {isOwner ? <button onClick={openConfirmation} className={`${styles['button']} ${styles['delete-button']}`}>Delete</button> : ''}
             {isOwner ? <button onClick={openEdit} className={`${styles['button']} ${styles['edit-button']}`}>Edit</button> : ''}
         </div>
